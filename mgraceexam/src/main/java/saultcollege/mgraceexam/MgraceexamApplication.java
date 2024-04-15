6b65a722fa1520272f219ed2a37f21223f834c69
mgviaje <113563698+mgviaje@users.noreply.github.com>
2024-04-15 12:08:14 -0700

    Initial commit

---
+++ mgraceexam/src/main/java/saultcollege/mgraceexam/MgraceexamApplication.java
@@ -0,0 +1,13 @@
+package saultcollege.mgraceexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgraceexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgraceexamApplication.class, args);
+	}
+
+}
---
+++ mgraceexam/src/test/java/saultcollege/mgraceexam/MgraceexamApplicationTests.java
@@ -0,0 +1,13 @@
+package saultcollege.mgraceexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgraceexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
